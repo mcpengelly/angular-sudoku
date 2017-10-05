a9eb142828e81ab690fee269d378b2db53b57696
mcpengelly <pengelly.mat@gmail.com>
2017-10-05 18:26:54 -0400

    propogate given values through the grid

--- src/app/solver.service.ts
+++ src/app/solver.service.ts
@@ -1,7 +1,11 @@
 export class SolverService {
-  count = 0;
   constructor() {}
+  checkValidCount = 0;
 
+  /**
+  * checks every cell in the given row to see if it can take the input value
+  * @return [boolean] if input is valid return true, otherwise return false
+  */
   checkRow(board, row, checkValue){
     if(row > 8) throw new Error('max index is 8');
     var validInput = true;
@@ -15,6 +19,10 @@ export class SolverService {
     return validInput;
   }
 
+  /**
+  *  checks every cell in the given column
+  *  @return [boolean] if input is valid return true, otherwise return false
+  */
   checkColumn(board, column, checkValue){
     if(column > 8) throw new Error('max index is 8');
     var validInput = true;
@@ -28,6 +36,10 @@ export class SolverService {
     return validInput;
   }
 
+  /**
+  *  checks every cell within the same 3x3 area as the cell
+  *  @return [boolean] if input is valid return true, otherwise return false
+  */
   check3x3Square(board, row, column, checkValue){
     if(row > 8 || column > 8) throw new Error('max index is 8');
     var validInput = true;
@@ -47,7 +59,10 @@ export class SolverService {
     return validInput;
   }
 
-  //returns a 2d array of [row,col] coords [[2,4],[4,8]]
+  /**
+   * creates a collection of unsolved sudoku square coords, alongside possible choices
+   * @return Array of objects
+   */
   findEmptySquares(board){
     var emptySquares = [];
 
@@ -55,7 +70,14 @@ export class SolverService {
       for(var j = 0; j < board.length; j++){
         // push the coordinates of each square location to an array
         if(board[i][j] === 0) {
-          emptySquares.push([i,j]);
+          var choices = this.generatePossibleChoices(board, i, j);
+
+          emptySquares.push({
+            row: i,
+            column: j,
+            choices: choices,
+            solveSpace: choices.slice() // clone choices for choiceSpace
+          });
         }
       }
     }
@@ -63,7 +85,7 @@ export class SolverService {
   }
 
   checkValidPlacement(board, row, column, value){
-    this.count++;
+    this.checkValidCount++;
     if(
     this.checkRow(board, row, value) &&
     this.checkColumn(board, column, value) &&
@@ -73,44 +95,96 @@ export class SolverService {
     return false;
   }
 
+  /**
+   * scans the board for Given values, then eliminates them as options
+   * for the row, column and 3x3square that the Given value belongs to
+   * @param {[type]} board  [description]
+   * @param {[type]} row    [description]
+   * @param {[type]} column [description]
+   * @return Array of possibleChoices representing the remaining possible inputs for the given cell
+   */
+  generatePossibleChoices(board, row, column){
+    var numPool = {};
+
+    // check rows spaces
+    for(var i = 0; i < board.length; i++){
+      for(var j = 0; j < board.length; j++){
+        if(board[row][j] !== 0){
+          numPool[board[row][j] + ''] = true;
+        }
+      }
+    }
+
+    // check columns spaces
+    for(var i = 0; i < board.length; i++){
+      for(var j = 0; j < board.length; j++){
+        if(board[i][column] !== 0){
+          numPool[board[i][column] + ''] = true;
+        }
+      }
+    }
+
+    // check 3x3 spaces
+    var startRowIndex = row - row % 3;
+    var startColumnIndex = column - column % 3;
+
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 3; j++){
+        if(board[startRowIndex + i][startColumnIndex + j] !== 0){
+          numPool[board[startRowIndex + i][startColumnIndex + j] + ''] = true;
+        }
+      }
+    }
+
+    var possibleChoices = [];
+    for(var i=1; i<=9; i++) {
+      var numKey = i + '';
+      if(!numPool[numKey]) {
+        possibleChoices.push(parseInt(numKey));
+      }
+    }
+
+    return possibleChoices;
+  }
+
+  /**
+   * takes an unsolved sudoku board and returns a solved one
+   */
   solve(sBoard){
     console.log('--- Original Board ---');
-    console.log(sBoard)
-    var board = sBoard.map((x)=>x.map((x)=>x)); // clone the board
-
-    // distinguish which spaces need to be filled
+    console.log(sBoard);
+    var board = sBoard.map((x)=>x.map((x)=>x));
+    // we need to distinguish which spaces need to be filled
     var emptySquares = this.findEmptySquares(board);
-    var limit = 9;
+
     var value;
-
-    //go through empty squares,
     for(var i = 0; i < emptySquares.length;){
+      var row = emptySquares[i].row;
+      var column = emptySquares[i].column;
       var found = false;
-      var row = emptySquares[i][0];
-      var column = emptySquares[i][1];
-      value = board[row][column] + 1;
 
-      while(!found && value <= limit){
+      while(!found){
+        // if there are no choices left, reset the possible choices for this space and backtrack
+        if(emptySquares[i].choices.length === 0){
+          emptySquares[i].choices = emptySquares[i].solveSpace.slice();
+          board[row][column] = 0;
+          i--;
+          break;
+        }
+
+        value = emptySquares[i].choices.shift();
+
         if(this.checkValidPlacement(board, row, column, value)){
           found = true;
           board[row][column] = value;
           i++;
         }
-        else {
-          value++;
-        }
-      }
-
-      //backtrack if no value fits
-      if(!found){
-        board[row][column] = 0;
-        i--;
       }
     }
 
+    console.log('checkValidCount:', this.checkValidCount);
     console.log('--- Solved Board ---');
     console.log(board);
-    console.log('count:', this.count);
     return board;
   }
 }
